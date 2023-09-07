aab5a74897eaf04c885d5a4ad20c96778f381179
Jet <jwoot001@gmail.com>
2023-09-07 21:27:22 +0800

    edit

--- src/utils/experience/experience.ts
+++ src/utils/experience/experience.ts
@@ -3,87 +3,87 @@
 //
 
 export const prof_experience = [
-	{
-		title: "Game Tester",
-		company: "Freelance",
-		link: "https://daqa.net/",
-		date: "Nov 2021 - Jan 2023",
-		exp: [
-			{
-				keyword: "Consistent Project Involvement:",
-				description:
-					"Actively participated in an average of three projects per week, conducting functional testing and beta testing to ensure product quality and reliability.",
-			},
-			{
-				keyword: "Proactive Learning and Collaboration:",
-				description:
-					" Engaged in proactive learning attending QA trainings to enhance efficiency and drive continuous improvement in quality assurance processes.",
-			},
-		],
-	},
-	{
-		title: "IT Operations Specialist",
-		company: "PNB - Head Office",
-		link: "https://www.pnb.com.ph/",
-		date: "Mar - Nov 2022",
-		exp: [
-			{
-				keyword: "Automation Script Development:",
-				description:
-					" Developed and implemented automation scripts for daily operations, resulting in a 40% reduction in manual work.",
-			},
-			{
-				keyword: "System Optimization:",
-				description:
-					"Successfully managed open system operations, optimizing the performance and functionality of various systems and processes, leading to improved efficiency and enhanced productivity.",
-			},
-			{
-				keyword: "Incident Report Management:",
-				description:
-					"Proactively handled and meticulously documented incident reports related to the operations department's responsibilities, ensuring swift issue resolution and increased transparency in workflow.",
-			},
-		],
-	},
-	{
-		title: "Web Developer",
-		company: "Freelance",
-		link: "#",
-		date: "July - Mar 2022",
-		exp: [
-			{
-				keyword: "Enhanced Front-End Skills:",
-				description:
-					"Improved my experience in HTML, CSS, and JavaScript for creating user-friendly web interfaces.",
-			},
-			{
-				keyword: "Learned PHP & Javascript",
-				description: "Create and maintain web applications using PHP7.0 and Javascript.",
-			},
-			{
-				keyword: "Developed Automation Scripts:",
-				description:
-					"Create discord bots/automation scripts for daily operations regarding NFTs (Non-Fungible Tokens) games.",
-			},
-		],
-	},
-	{
-		title: "Web Developer Training",
-		company: "Qualis Consulting, Inc",
-		link: "https://www.linkedin.com/company/qualis-consulting-inc/?originalSubdomain=ph",
-		date: "Apr - July 2021",
-		exp: [
-			{
-				keyword: "Front-End Proficiency:",
-				description: "Gained experience in HTML, CSS, and JavaScript for creating user-friendly web interfaces.",
-			},
-			{
-				keyword: "Developed PL/SQL:",
-				description: "Developed skills in PL/SQL for efficient database management and server-side logic.",
-			},
-			{
-				keyword: "Selenium Automation Learning:",
-				description: "Learned Selenium automation scripting to enhance testing efficiency and product quality.",
-			},
-		],
-	},
+    {
+        title: "Game Tester",
+        company: "Freelance",
+        link: "https://daqa.net/",
+        date: "Nov 2021 - Mar 2023",
+        exp: [
+            {
+                keyword: "Consistent Project Involvement:",
+                description:
+                    "Actively participated in an average of three projects per week, conducting functional testing and beta testing to ensure product quality and reliability.",
+            },
+            {
+                keyword: "Proactive Learning and Collaboration:",
+                description:
+                    " Engaged in proactive learning attending QA trainings to enhance efficiency and drive continuous improvement in quality assurance processes.",
+            },
+        ],
+    },
+    {
+        title: "IT Operations Specialist",
+        company: "PNB - Head Office",
+        link: "https://www.pnb.com.ph/",
+        date: "Mar - Nov 2022",
+        exp: [
+            {
+                keyword: "Automation Script Development:",
+                description:
+                    " Developed and implemented automation scripts for daily operations, resulting in a 40% reduction in manual work.",
+            },
+            {
+                keyword: "System Optimization:",
+                description:
+                    "Successfully managed open system operations, optimizing the performance and functionality of various systems and processes, leading to improved efficiency and enhanced productivity.",
+            },
+            {
+                keyword: "Incident Report Management:",
+                description:
+                    "Proactively handled and meticulously documented incident reports related to the operations department's responsibilities, ensuring swift issue resolution and increased transparency in workflow.",
+            },
+        ],
+    },
+    {
+        title: "Web Developer",
+        company: "Freelance",
+        link: "#",
+        date: "July - Mar 2022",
+        exp: [
+            {
+                keyword: "Enhanced Front-End Skills:",
+                description:
+                    "Improved my experience in HTML, CSS, and JavaScript for creating user-friendly web interfaces.",
+            },
+            {
+                keyword: "Learned PHP & Javascript",
+                description: "Create and maintain web applications using PHP7.0 and Javascript.",
+            },
+            {
+                keyword: "Developed Automation Scripts:",
+                description:
+                    "Create discord bots/automation scripts for daily operations regarding NFTs (Non-Fungible Tokens) games.",
+            },
+        ],
+    },
+    {
+        title: "Web Developer Training",
+        company: "Qualis Consulting, Inc",
+        link: "https://www.linkedin.com/company/qualis-consulting-inc/?originalSubdomain=ph",
+        date: "Apr - July 2021",
+        exp: [
+            {
+                keyword: "Front-End Proficiency:",
+                description: "Gained experience in HTML, CSS, and JavaScript for creating user-friendly web interfaces.",
+            },
+            {
+                keyword: "Developed PL/SQL:",
+                description: "Developed skills in PL/SQL for efficient database management and server-side logic.",
+            },
+            {
+                keyword: "Selenium Automation Learning:",
+                description: "Learned Selenium automation scripting to enhance testing efficiency and product quality.",
+            },
+        ],
+    },
 ];
